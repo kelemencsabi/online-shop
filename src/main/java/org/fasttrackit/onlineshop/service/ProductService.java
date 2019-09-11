0683f58630933f3b228a3c0ee354bd61a0c5436a
Csabi <csabykelemen@gmail.com>
2019-09-11 21:12:53 +0300

    added product service and create product tests

---
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -0,0 +1,39 @@
+package org.fasttrackit.onlineshop.service;
+
+import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.persistance.ProductRepository;
+import org.fasttrackit.onlineshop.transfer.product.SaveProductRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
+
+    //dependency injection
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(SaveProductRequest request){
+        LOGGER.info("creating product: {}",request);
+
+        Product product = new Product();
+        product.setName(request.getName());
+        product.setPrice(request.getPrice());
+        product.setDescription(request.getDescription());
+        product.setQuantity(request.getQuantity());
+        product.setImagePath(request.getImagePath());
+
+        return productRepository.save(product);
+
+    }
+
+
+}
