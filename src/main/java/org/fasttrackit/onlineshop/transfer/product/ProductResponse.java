a52c994364efb7509911a86d75874cfd2f274057
Csabi <csabykelemen@gmail.com>
2019-10-09 21:31:48 +0300

    fixed lazy loading for getProducts

---
+++ src/main/java/org/fasttrackit/onlineshop/transfer/product/ProductResponse.java
@@ -0,0 +1,90 @@
+package org.fasttrackit.onlineshop.transfer.product;
+
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+public class ProductResponse {
+    private long id;
+
+    @NotNull
+    private String name;
+    @NotNull
+    private int quantity;
+    @NotNull
+    private double price;
+    private String description;
+    private String imagePath;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductResponse{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", imagePath='" + imagePath + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductResponse that = (ProductResponse) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
+++ src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
@@ -3,6 +3,7 @@ package org.fasttrackit.onlineshop.web;
 import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.service.ProductService;
 import org.fasttrackit.onlineshop.transfer.product.GetProductsRequest;
+import org.fasttrackit.onlineshop.transfer.product.ProductResponse;
 import org.fasttrackit.onlineshop.transfer.product.SaveProductRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -37,8 +38,8 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<org.springframework.data.domain.Page<Product>> getProducts(GetProductsRequest request, Pageable pageable) {
-        Page<Product> products = productService.getProducts(request, pageable);
+    public ResponseEntity<org.springframework.data.domain.Page<ProductResponse>> getProducts(GetProductsRequest request, Pageable pageable) {
+        Page<ProductResponse> products = productService.getProducts(request, pageable);
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
